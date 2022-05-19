9d56fc20ef46dfdc9c31c3cd4bdb1a824b141c22
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-05-19 13:44:42 +0200

    update

--- codons_02_bases_manipulation_donnees.R
+++ codons_02_bases_manipulation_donnees.R
@@ -5,137 +5,123 @@
 # Charger les packages ----
 
 library(dplyr)
+library(tidyr)
 
 # Definir le repertoire de travail ----
 
-setwd("D:/codons/Codons-01-PremiersPasAvecR/")
+setwd("C-02-ManipulationDonneesBases/")
 
 # Importer les donnees ----
 
-oiseaux <- read.csv("oiseaux.csv")
+croissance <- read.csv("croissance.csv")
 
 # Explorer les donnees ----
 
-head(oiseaux)  # affiche les premieres lignes
-tail(oiseaux)  # affiche les dernieres lignes
-str(oiseaux)  # affiche la structure des donnees
+head(croissance)  # affiche les premieres lignes
+str(croissance)  # affiche les types des variables
+croissance$Indiv  # affiche tous les elements de la variable Indiv
+length(unique(croissance$Indiv))  # nombre d'arbrisseaux dans le jeu de donnees
 
-head(oiseaux$categorie)  # affiche les premiers elements de la variable
-class(oiseaux$categorie)  # indique le type de variable
-oiseaux$categorie <- as.factor(oiseaux$categorie)  # transforme la variable en facteur
-class(oiseaux$categorie)  # verification
+# Selectionner des donnees ----
 
-dim(oiseaux)  # affiche les dimensions de l'objet (x lignes par y colonnes)
-summary(oiseaux)  # synthese de l'objet
-summary(oiseaux$categorie)  # synthese de la variable
+croissance[2, 5]  # 2e ligne et 5e colonne
+croissance[6, ]  # 6e ligne
+croissance[6, ]$Indiv  # contenu de la colonne Indiv pour la 6e ligne
+croissance[croissance$Indiv == 603, ]  # toutes les colonnes pour l'individu n°603
 
-# Compter le nombre d'especes par niveau de preoccupation ----
+croissance[croissance$Zone < 4, ]  # zones 2 et 3
+croissance[croissance$Zone <= 4, ]  # zones 2, 3 et 4
+croissance[!croissance$Zone >= 5, ]  # meme resultat
 
-en_danger_critique <- filter(oiseaux, categorie == "En danger critique")
-en_danger <- filter(oiseaux, categorie == "En danger")
-vulnerable <- filter(oiseaux, categorie == "Vulnerable")
-quasi_menacee <- filter(oiseaux, categorie == "Quasi-menacee")
-preoccupation_mineure <- filter(oiseaux, categorie == "Preoccupation mineure")
+croissance[croissance$Zone == 2 | croissance$Zone == 7, ]  # zones 2 et 7
+croissance[croissance$Zone == 2 & croissance$Indiv %in% c(300:400), ]  # zones 2 et individus 300 a 400
 
-a <- length(unique(en_danger_critique$nom_francais))
-b <- length(unique(en_danger$nom_francais))
-c <- length(unique(vulnerable$nom_francais))
-d <- length(unique(quasi_menacee$nom_francais))
-e <- length(unique(preoccupation_mineure$nom_francais))
+# Creer et ecraser des objets ----
 
-comptage_especes <- c(a, b, c, d, e)
-names(comptage_especes) <- c("En danger critique",
-                             "En danger",
-                             "Vulnerable",
-                             "Quasi-menacee",
-                             "Preoccupation mineure")
+croissance2 <- croissance  # cree une copie de travail
+names(croissance2)  # affiche les noms de colonnes
+names(croissance2)[1] <- "zone"  # modifie le nom de la 1e colonne : "Zone" devient "zone"
+names(croissance2)[2] <- "ID"  # modifie le nom de la 2e colonne : "Indiv" devient "ID
 
-# Representer les donn?es ----
+croissance2[1, 4] <- 5.7  # remplace la valeur 5.1 par 5.7 dans la 1e ligne - 4e colonne
+croissance2[croissance2$ID == 373, ]$X2008 <- 5.7  # meme resultat
 
-barplot(comptage_especes)
+croissance2$zone <- as.factor(croissance2$zone)  # transformer la variable zone en facteur
+str(croissance2)
+levels(croissance2$zone) <- c("A", "B", "C", "D", "E", "F")  # modifier les niveaux du facteur
 
-help(barplot)  # affiche l'aide de la fonction
-?barplot
+# Donnees tidy ----
 
-# Sauvegarder le graphique ----
+croissance_long <- pivot_longer(  # format large -> format long
+  data = croissance,  # objet a transformer
+  cols = c(X2007, X2008, X2009, X2010, X2011, X2012),  # colonnes a regrouper
+  names_to = "Annee",  # les annees sont regroupees dans cette colonne
+  values_to = "Croissance")  # les valeurs mesurees sont regroupees dans cette colonne
 
-png("barplot.png",
-    width = 1600, height = 600)
+croissance_large <- pivot_wider(  # format long -> format large
+  data = croissance_long,  # objet a transformer
+  id_cols = c(Zone, Indiv),  # colonnes qui contiennent les identifiants
+  names_from = Annee,  # cette colonne sert a creer les noms de colonnes
+  values_from = Croissance)  # cette colonne sert a remplir les colonnes
 
-barplot(comptage_especes,
-        xlab = "Niveau de menace", ylab = "Nombre d'esp?ces",
-        ylim = c(0, 70),
-        cex.names = 1.5, cex.axis = 1.5, cex.lab = 1.5)
+croissance_long2 <- pivot_longer(
+  data = croissance,
+  cols = c(3:8),  # numeros de colonnes
+  names_to = "Annee",
+  values_to = "Croissance")
 
-dev.off()
+# Boxplot de la croissance annuelle ----
 
-# Creer un dataframe ----
+boxplot(Croissance ~ Annee,  # 1 boxplot par annee
+        data = croissance_long,
+        main = "Croissance annuelle de Empetrum hermaphroditum",
+        ylab = "Croissance (cm)")
 
-categories <- c("En danger critique",
-                "En danger",
-                "Vulnerable",
-                "Quasi-menacee",
-                "Preoccupation mineure")
+# rename() ----
 
-categories_f <- factor(categories)
+croissance_long <- rename(croissance_long,  # objet
+                          zone = Zone,  # renomme "Zone" en "zone"
+                          indiv = Indiv,  # renomme "Indiv" en "indiv"
+                          annee = Annee,  # renomme "Annee" en "annee"
+                          croissance = Croissance)  # renomme "Croissance" en "croissance"
 
-comptage <- c(a, b, c, d, e)
+# filter() ----
 
-especes_menacees <- data.frame(categories_f, comptage)  # cr?er le data frame
+croissance_selection <- filter(croissance_long,  # objet
+                               zone %in% c(2, 3),  # garder les zones 2 et 3
+                               annee %in% c("X2009", "X2010", "X2011"))  # garder les annees 2009 a 2011
 
-write.csv(especes_menacees, "especes_menacees.csv")
+# select() ----
 
-# Representation graphique ----
+croissance_sans_zone <- select(croissance_long,  # objet
+                               indiv, annee, croissance)  # selectionner ces trois colonnes
 
-png("barplot2.png",
-    width = 1600, height = 600)
+croissance_sans_zone <- select(croissance_long,  # objet
+                               -zone)  # supprimer cette colonne
 
-barplot(especes_menacees$comptage,
-        names.arg = c("En danger critique",
-                      "En danger",
-                      "Vulnerable",
-                      "Quasi-menacee",
-                      "Preoccupation mineure"),
-        xlab = "Niveau   de menace", ylab = "Nombre d'especes",
-        ylim = c(0, 70),
-        cex.names = 1.5, cex.axis = 1.5, cex.lab = 1.5)
+croissance_sans_zone <- select(croissance_long,  # objet
+                               Annee = annee,  # selectionne et renomme la colonne
+                               ID.arbrisseau = indiv,
+                               Croissance = croissance)
 
-# Defi ----
+# mutate() ----
 
-# Extraire les envergures pour chaque espece
-env_aigle <- c(195, 201, 185)
-env_chouette <- c(85, 102, 91)
-env_colibri <- c(8, 9, 9)
-env_moineau <- c(24, 21, 22)
+croissance_totale <- mutate(croissance,  # objet
+                            croissance.totale = X2007 + X2008 + X2009 + X2010 + X2011 + X2012)  # somme des colonnes
 
-# Calculer l'envergure moyenne
-env_moy_aigle <- mean(env_aigle)
-env_moy_chouette <- mean(env_chouette)
-env_moy_colibri <- mean(env_colibri)
-env_moy_moineau <- mean(env_moineau)
+# group_by() ----
 
-# Rassembler ces valeurs moyennes dans un vecteur
-env_moyenne <- c(env_moy_aigle, env_moy_chouette, env_moy_colibri, env_moy_moineau)
+croissance_gp <- group_by(croissance_long, indiv)  # regrouper les donnees par individu
 
-# Creer un vecteur avec les noms des especes (attention ? l'ordre !)
-especes <- c("Aigle", "Chouette", "Colibri", "Moineau")
+# summarise() ----
 
-# Transformer ce vecteur en un facteur
-especes_f <- as.factor(especes)
-class(especes_f)
+synthese1 <- summarise(croissance_long,  # objet
+                       croissance.totale = sum(croissance))  # ajouter une colonne avec la somme des croissances
 
-# Combiner les deux vecteurs en un data frame
-envergure <- data.frame(especes_f, env_moyenne)
+synthese2 <- summarise(croissance_gp,  # objet
+                       croissance.totale = sum(croissance))  # somme des croissances par individu
 
-# Sauvegarder le barplot dans un fichier
-png("barplot_envergure.png",
-    width = 1600, height = 600)
-
-barplot(envergure$env_moyenne,
-        names.arg = envergure$especes_f,
-        xlab = "Esp?ces", ylab = "Envergure moyenne",
-        ylim = c(0, 200),
-        cex.names = 1.5, cex.axis = 1.5, cex.lab = 1.5,
-        col = "gold")  # modifier la couleur des barres
-
-dev.off()
+synthese3 <- summarise(croissance_gp,
+                       total.croissance = sum(croissance),
+                       moyenne.croissance = mean(croissance),
+                       ecart.type.croissance = sd(croissance))
