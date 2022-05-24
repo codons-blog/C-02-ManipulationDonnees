7e51021b94908415a6fc0d69411ddafc056c0d46
codons-blog <codons@protonmail.com>
2022-05-24 16:49:28 +0200

    added boxplot

--- codons_02_manipulation_donnees.R
+++ codons_02_manipulation_donnees.R
@@ -84,18 +84,6 @@ croissance2 %>% filter(zone == 2, indiv %in% 300:400)
 croissance2 %>% filter(zone == 2, between(indiv, 300, 400))
 
 
-# Selectionner des donnees ----
-
-
-croissance[croissance$Zone == 2 | croissance$Zone == 7, ]  # zones 2 et 7
-croissance[croissance$Zone == 2 & croissance$Indiv %in% c(300:400), ]  # zones 2 et individus 300 a 400
-
-# Creer et ecraser des objets ----
-
-croissance2 <- croissance  # cree une copie de travail
-names(croissance2)  # affiche les noms de colonnes
-names(croissance2)[1] <- "zone"  # modifie le nom de la 1e colonne : "Zone" devient "zone"
-names(croissance2)[2] <- "ID"  # modifie le nom de la 2e colonne : "Indiv" devient "ID
 
 croissance2[1, 4] <- 5.7  # remplace la valeur 5.1 par 5.7 dans la 1e ligne - 4e colonne
 croissance2[croissance2$ID == 373, ]$X2008 <- 5.7  # meme resultat
@@ -104,93 +92,110 @@ croissance2$zone <- as.factor(croissance2$zone)  # transformer la variable zone 
 str(croissance2)
 levels(croissance2$zone) <- c("A", "B", "C", "D", "E", "F")  # modifier les niveaux du facteur
 
-# Donnees tidy ----
+# arrange() ----
 
-croissance_long <- pivot_longer(  # format large -> format long
-  data = croissance,  # objet a transformer
-  cols = c(X2007, X2008, X2009, X2010, X2011, X2012),  # colonnes a regrouper
-  names_to = "Annee",  # les annees sont regroupees dans cette colonne
-  values_to = "Croissance")  # les valeurs mesurees sont regroupees dans cette colonne
+# Trier par ordre croissant sur l'annee 2007
 
-croissance_large <- pivot_wider(  # format long -> format large
-  data = croissance_long,  # objet a transformer
-  id_cols = c(Zone, Indiv),  # colonnes qui contiennent les identifiants
-  names_from = Annee,  # cette colonne sert a creer les noms de colonnes
-  values_from = Croissance)  # cette colonne sert a remplir les colonnes
+croissance2 %>% arrange(`2007`)
 
-croissance_long2 <- pivot_longer(
-  data = croissance,
-  cols = c(3:8),  # numeros de colonnes
-  names_to = "Annee",
-  values_to = "Croissance")
+# Trier par ordre decroissant sur l'annee 2008
 
-# Boxplot de la croissance annuelle ----
-
-boxplot(Croissance ~ Annee,  # 1 boxplot par annee
-        data = croissance_long,
-        main = "Croissance annuelle de Empetrum hermaphroditum",
-        ylab = "Croissance (cm)")
-
-# rename() ----
-
-croissance_long <- rename(croissance_long,  # objet
-                          zone = Zone,  # renomme "Zone" en "zone"
-                          indiv = Indiv,  # renomme "Indiv" en "indiv"
-                          annee = Annee,  # renomme "Annee" en "annee"
-                          croissance = Croissance)  # renomme "Croissance" en "croissance"
-
-# filter() ----
-
-croissance_selection <- filter(croissance_long,  # objet
-                               zone %in% c(2, 3),  # garder les zones 2 et 3
-                               annee %in% c("X2009", "X2010", "X2011"))  # garder les annees 2009 a 2011
-
-# select() ----
-
-croissance_sans_zone <- select(croissance_long,  # objet
-                               indiv, annee, croissance)  # selectionner ces trois colonnes
-
-croissance_sans_zone <- select(croissance_long,  # objet
-                               -zone)  # supprimer cette colonne
-
-croissance_sans_zone <- select(croissance_long,  # objet
-                               Annee = annee,  # selectionne et renomme la colonne
-                               ID.arbrisseau = indiv,
-                               Croissance = croissance)
+croissance2 %>% arrange(desc(`2008`))
 
 # mutate() ----
 
-croissance_totale <- mutate(croissance,  # objet
-                            croissance.totale = X2007 + X2008 + X2009 + X2010 + X2011 + X2012)  # somme des colonnes
+# Calculer la croissance totale pour chaque individu entre 2007 et 2012
+
+croissance2_totale <- croissance2 %>% 
+  mutate(croissance.totale = `2007` + `2008` + `2009` + `2010` + `2011` + `2012`)
 
 # group_by() ----
 
-croissance_gp <- group_by(croissance_long, indiv)  # regrouper les donnees par individu
+croissance2_groupes <- croissance2 %>% 
+  group_by(zone)
+
+head(croissance2)
+head(croissance2_groupes)
 
 # summarise() ----
 
-synthese1 <- summarise(croissance_long,  # objet
-                       croissance.totale = sum(croissance))  # ajouter une colonne avec la somme des croissances
+synthese1 <- croissance2 %>% 
+  summarise(croissance.totale.2007 = sum(`2007`))
 
-synthese2 <- summarise(croissance_gp,  # objet
-                       croissance.totale = sum(croissance))  # somme des croissances par individu
+synthese2 <- croissance2_groupes %>% 
+  summarise(croissance.totale.2007 = sum(`2007`))
 
-synthese3 <- summarise(croissance_gp,
-                       total.croissance = sum(croissance),
-                       moyenne.croissance = mean(croissance),
-                       ecart.type.croissance = sd(croissance))
+synthese3 <- croissance2_groupes %>% 
+  summarise(croissance.totale.2007 = sum(`2007`),
+            croissance.moyenne.2007 = mean(`2007`),
+            croissance.ecart.type.2007 = sd(`2007`)) 
 
 # join() ----
 
-traitements <- read.csv2("traitements.csv")  # importer le fichier
-head(traitements)  # afficher les 6 premieres lignes
+# Importer le fichier traitements.csv
 
-expe <- left_join(croissance_long, traitements,  # tableau d'origine + tableau a joindre
-                  by = c("indiv" = "Indiv", "zone" = "Zone"))  # colonnes communes aux 2 tableaux
+traitements <- read_csv2("https://raw.githubusercontent.com/codons-blog/C-02-ManipulationDonnees/main/traitements.csv")  # importer le fichier
 
-expe2 <- merge(croissance_long, traitements,  # meme resultat en R basique
-               by.x = c("zone", "indiv"),
-               by.y = c("Zone", "Indiv"))
+# Joindre les deux jeux de données
 
-boxplot(croissance ~ Treatment,
-        data = expe)
+expe <- croissance2 %>% 
+  left_join(traitements,
+            by = c("zone" = "Zone",
+                   "indiv" = "Indiv"))
+
+# pivot_longer() ----
+
+croissance_long <- croissance %>% 
+  pivot_longer(cols = `2007`:`2012`,
+               names_to = "Annee",
+               values_to = "Croissance")
+
+# pivot_wider() ----
+
+croissance_large <- croissance_long %>% 
+  pivot_wider(id_cols = Zone:Indiv,
+              names_from = Annee,
+              values_from = Croissance)
+
+# pipe ----
+
+croissance <- read_csv("https://raw.githubusercontent.com/codons-blog/C-02-ManipulationDonnees/main/croissance.csv")
+traitements <- read_csv2("https://raw.githubusercontent.com/codons-blog/C-02-ManipulationDonnees/main/traitements.csv")  # importer le fichier
+
+expe <- croissance %>% 
+  pivot_longer(cols = `2007`:`2012`,
+               names_to = "annee",
+               values_to = "croissance") %>% 
+  left_join(traitements) %>% 
+  rename(zone = Zone,
+         indiv = Indiv,
+         traitement = Traitement) %>% 
+  mutate(zone = as.factor(zone),
+         indiv = as.factor(indiv),
+         annee = as.numeric(annee),
+         traitement = as.factor(traitement))
+
+boxplot(croissance ~ traitement, data = expe)
+
+  
+expe
+
+
+# Boxplot de la croissance annuelle ----
+
+png("boxplot.png",
+    width = 1600, height = 600)
+
+boxplot(croissance ~ traitement, 
+        data = expe,
+        main = "Effet de la température (T) et de la fertilisation (F) sur la croissance d'Empetrum",
+        xlab = "Traitement",
+        ylab = "Croissance des tiges (cm)")
+
+dev.off()
+
+
+
+
+
+
