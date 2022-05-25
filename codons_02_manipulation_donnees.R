2f97f8766bf7d0a025ea0e88fa857b6627b1f26e
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-05-25 10:17:00 +0200

    updated script for 2nd tutorial

--- codons_02_manipulation_donnees.R
+++ codons_02_manipulation_donnees.R
@@ -23,11 +23,7 @@ length(unique(croissance$Indiv))  # nombre d'arbrisseaux dans le jeu de donnees
 
 # rename() ----
 
-croissance2 <- rename(croissance,
-                      zone = Zone,
-                      indiv = Indiv)
-
-croissance2 <- croissance %>% 
+croissance <- croissance %>% 
   rename(zone = Zone,
          indiv = Indiv)
 
@@ -35,97 +31,92 @@ croissance2 <- croissance %>%
 
 # Conserver des colonnes
 
-croissance2_selection <- croissance2 %>% 
+croissance_selection <- croissance %>% 
   select(indiv, `2007`:`2012`)
 
 # Supprimer une colonne
 
-croissance2_selection <- croissance2 %>% 
+croissance_selection <- croissance %>% 
   select(-zone)
 
 # Renommer et modifier l'ordre de colonnes
 
-croissance2_selection <- croissance %>% 
-  select(zone = Zone, indiv = Indiv, `2007`:`2012`)
+croissance_selection <- croissance %>% 
+  select(zone, id = indiv, `2007`:`2012`)
 
 # Meme chose avec la fonction everything
 
-croissance2_selection <- croissance %>% 
-  select(zone = Zone, indiv = Indiv, everything())
+croissance_selection <- croissance %>% 
+  select(zone, id = indiv, everything())
 
 # filter() ----
 
-# Conserver les observations pour les zones 2 et 3 et les individus dont les identifiants sont compris entre 350 et 450
+# individu n°603
 
-croissance2_filtre <- croissance2 %>% 
-  filter(zone %in% c(2, 3),
-         indiv %in% 350:450)
+croissance %>% filter(indiv == 603)
 
-# Autre possibilité avec la fonction between()
+# zones 2, 3 et 4
 
-croissance2_filtre <- croissance2 %>% 
-  filter(zone %in% c(2, 3),
-         between(indiv, 350, 450))
+croissance %>% filter(zone <= 4)
+croissance %>% filter(zone %in% c(2, 3, 4))
+croissance %>% filter(zone %in% 2:4)
+croissance %>% filter(!zone >= 5)
 
-# Selectionner l'individu n°603
+# zones 2 et 7
 
-croissance2 %>% filter(indiv == 603)
+croissance %>% filter(zone == 2 | zone == 7)
+croissance %>% filter(zone %in% c(2, 7))
 
-# Selectionner les zones 2, 3 et 4
-croissance2 %>% filter(zone <= 4)
-croissance2 %>% filter(!zone >= 5)
+# zone 2 + individus entre 300 et 400
 
-# Selectionner les zones 2 et 7
-croissance2 %>% filter(zone == 2 | zone == 7)
-croissance2 %>% filter(zone %in% c(2, 7))
-
-# Selectionner les individus dans la zone 2 avec des identifiants entre 300 et 400
-croissance2 %>% filter(zone == 2, indiv %in% 300:400)
-croissance2 %>% filter(zone == 2, between(indiv, 300, 400))
-
-
-
-croissance2[1, 4] <- 5.7  # remplace la valeur 5.1 par 5.7 dans la 1e ligne - 4e colonne
-croissance2[croissance2$ID == 373, ]$X2008 <- 5.7  # meme resultat
-
-croissance2$zone <- as.factor(croissance2$zone)  # transformer la variable zone en facteur
-str(croissance2)
-levels(croissance2$zone) <- c("A", "B", "C", "D", "E", "F")  # modifier les niveaux du facteur
+croissance %>% filter(zone == 2 & indiv %in% 300:400)
+croissance %>% filter(zone == 2 & between(indiv, 300, 400))
 
 # arrange() ----
 
 # Trier par ordre croissant sur l'annee 2007
 
-croissance2 %>% arrange(`2007`)
+croissance %>% arrange(`2007`)
 
 # Trier par ordre decroissant sur l'annee 2008
 
-croissance2 %>% arrange(desc(`2008`))
+croissance %>% arrange(desc(`2008`))
 
 # mutate() ----
 
 # Calculer la croissance totale pour chaque individu entre 2007 et 2012
 
-croissance2_totale <- croissance2 %>% 
+croissance_totale <- croissance %>% 
   mutate(croissance.totale = `2007` + `2008` + `2009` + `2010` + `2011` + `2012`)
+
+croissance_totale <- croissance %>% 
+  mutate(zone = as.factor(zone),  # definir les 2 premieres colonnes comme etant des facteurs
+         indiv = as.factor(indiv)) %>% 
+  mutate(croissance.totale = rowSums(across(where(is.numeric))))  # calculer la somme des lignes pour les variables numeriques
 
 # group_by() ----
 
-croissance2_groupes <- croissance2 %>% 
+croissance_groupes <- croissance %>% 
   group_by(zone)
 
-head(croissance2)
-head(croissance2_groupes)
+head(croissance)
+head(croissance_groupes)
 
 # summarise() ----
 
-synthese1 <- croissance2 %>% 
+# Croissance totale pour l'ensemble des individus pour l'annee 2007
+
+synthese1 <- croissance %>% 
   summarise(croissance.totale.2007 = sum(`2007`))
 
-synthese2 <- croissance2_groupes %>% 
+# Croissance totale pour l'ensemble des individus groupes par zone pour l'annee 2007
+
+synthese2 <- croissance_groupes %>% 
   summarise(croissance.totale.2007 = sum(`2007`))
 
-synthese3 <- croissance2_groupes %>% 
+# Calcul de plusieurs parametres
+
+synthese3 <- croissance_groupes %>% 
   summarise(croissance.totale.2007 = sum(`2007`),
             croissance.moyenne.2007 = mean(`2007`),
             croissance.ecart.type.2007 = sd(`2007`)) 
@@ -134,11 +125,11 @@ synthese3 <- croissance2_groupes %>%
 
 # Importer le fichier traitements.csv
 
-traitements <- read_csv2("https://raw.githubusercontent.com/codons-blog/C-02-ManipulationDonnees/main/traitements.csv")  # importer le fichier
+traitements <- read_csv2("https://raw.githubusercontent.com/codons-blog/C-02-ManipulationDonnees/main/traitements.csv")
 
 # Joindre les deux jeux de données
 
-expe <- croissance2 %>% 
+expe <- croissance %>% 
   left_join(traitements,
             by = c("zone" = "Zone",
                    "indiv" = "Indiv"))
